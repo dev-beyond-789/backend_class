7dc89fb3dfc18264889167a45708b2e5cf0786a9
dev-beyond-789 <sarthak1110000@gmail.com>
2026-05-31 15:38:38 +0545

    variable

---
+++ javascript/variable.js
@@ -0,0 +1,19 @@
+// variable
+
+// let is used for certain scope
+
+{
+  let a = 7;
+  console.log(a);
+}
+
+// var is used globally.
+var b = 9;
+
+// const for every time no Change
+const c = 22;
+
+// print
+console.log(b);
+
+
