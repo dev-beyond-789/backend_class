d67656a826f9da99c69e35a21225a9493c672537
dev-beyond-789 <sarthak1110000@gmail.com>
2026-05-31 16:24:48 +0545

    operetor

---
+++ javascript/operator.js
@@ -0,0 +1,28 @@
+// arithmetic operator add subtract multiply and divide
+
+let num1 = 3;
+let num2 = 5;
+let sum = num1 + num2;
+console.log(sum);
+
+let sub = num1 - num2;
+console.log(sub);
+
+// logical operator
+//
+
+let a = 3;
+let b = 5;
+let result1 = a === b;
+let result2 = a == b;
+
+console.log(result1);
+console.log(result2);
+
+// increment
+
+let num = 4;
+let n = num++;
+let m = num--;
+console.log(n);
+console.log(m)
