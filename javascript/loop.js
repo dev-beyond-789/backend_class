31bfd86d5e5969ea430295a91741c256da658f2f
dev-beyond-789 <sarthak1110000@gmail.com>
2026-06-01 22:28:45 +0545

    loop

--- javascript/loop.js
+++ javascript/loop.js
@@ -1,8 +1,13 @@
 // for loop is defined as iteration until condn is satisfied
 
 let name = "hum";
-
 for (let i = 0;i<5; i++){
   console.log(name);
 }
+
+let name = "hum";
+for (let i = 0;i<5; i--){
+  console.log(name);
+}
+
 
