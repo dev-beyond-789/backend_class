0838b953b1da3a9767ae9d0cf14fc59518da1d6f
dev-beyond-789 <sarthak1110000@gmail.com>
2026-06-01 19:23:43 +0545

    assignment

--- javascript/assignment.js
+++ javascript/assignment.js
@@ -4,3 +4,5 @@ let num1 = 5;
 for (let i = 1; i <= 10; i++) {
   console.log(`${num1} * ${i} = `, num1 * i);
 }
+
+// using a arrow function print prime number upto 79
--- javascript/function.js
+++ javascript/function.js
@@ -41,3 +41,4 @@ console.log("okay")
 console.log("not okay")
   }
 }
+
