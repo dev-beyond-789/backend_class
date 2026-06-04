907aaa8fef64689149af614813740165494bd8b7
dev-beyond-789 <sarthak1110000@gmail.com>
2026-06-04 23:52:11 +0545

    assign

--- javascript/assignment.js
+++ javascript/assignment.js
@@ -1,13 +1,60 @@
 // print the multiple table of 5
 
-let num1 = 5;
-for (let i = 1; i <= 10; i++) {
-  console.log(`${num1} * ${i} = `, num1 * i);
-}
+// let num1 = 5;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${num1} * ${i} = `, num1 * i);
+// }
 
 // using a arrow function print prime number upto 79
 
-// const isprime = () =>{
-//   for(let i = 1; i <= 79; i++)
+// const prime = (num) => {
+//   for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       return true;
+//     }
+//   }
+//   return num > 1;
+// };
+
+// for (let i = 1; i <= 79; i++) {
+//   if (prime(i)) {
+//     console.log(i);
+//   }
 // }
-// console.log()
+
+// const isPrime = (num) => {
+//   let prime = true;
+
+//   for (let i = 2; i < num; i++) {
+//     prime = prime && !Number.isInteger(num / i);
+//   }
+
+//   return num > 1 && prime;
+// };
+
+// for (let i = 1; i <= 20; i++) {
+//   if (isPrime(i)) {
+//     console.log(i);
+//   }
+// }
+
+for (let num = 2; num <= 20; num++) {
+  let prime = true;
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      prime = false;
+    }
+  }
+
+  if (prime) {
+    console.log(num);
+  }
+}
+
+let a = 5;
+let b = 2;
+
+console.log(a % b);
+
+
