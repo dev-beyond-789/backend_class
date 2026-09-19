7df62071669160856dbca5e627fd2291fdd9c852
dev-beyond-789 <sarthak1110000@gmail.com>
2026-09-19 08:28:16 +0545

    assignment

--- javascript/assignment.js
+++ javascript/assignment.js
@@ -14,8 +14,16 @@
 // };
 
 // even no.s under 50
-for (let num = 1; num <= 50; num++) {
-    if (num % 2 === 0) {
+// for (let num = 1; num <= 50; num++) {
+//     if (num % 2 === 0) {
+//         console.log(num);
+//     }
+// }
+
+// multiples of 5 under 100
+
+for (let num = 1; num <= 100; num++) {
+    if (num % 5 === 0) {
         console.log(num);
     }
 }
