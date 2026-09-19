ed92e059d5e21a86ad117b0ea13274d5b77e82bf
dev-beyond-789 <sarthak1110000@gmail.com>
2026-09-19 08:24:12 +0545

    old assignment

--- javascript/assignment.js
+++ javascript/assignment.js
@@ -1,84 +1,14 @@
-// print the multiple table of 5
+// assignment one 
+for (let num = 2; num <= 70; num++) {
+    let prime = true;
 
-// let num1 = 5;
-// for (let i = 1; i <= 10; i++) {
-//   console.log(`${num1} * ${i} = `, num1 * i);
-// }
-
-// using a arrow function print prime number upto 79
-
-// const prime = (num) => {
-//   for (let i = 2; i < num; i++) {
-//     if (num % i === 0) {
-//       return true;
-//     }
-//   }
-//   return num > 1;
-// };
-
-// for (let i = 1; i <= 79; i++) {
-//   if (prime(i)) {
-//     console.log(i);
-//   }
-// }
-
-// const isPrime = (num) => {
-//   let prime = true;
-
-//   for (let i = 2; i < num; i++) {
-//     prime = prime && !Number.isInteger(num / i);
-//   }
-
-//   return num > 1 && prime;
-// };
-
-// for (let i = 1; i <= 20; i++) {
-//   if (isPrime(i)) {
-//     console.log(i);
-//   }
-// }
-
-for (let num = 2; num <= 20; num++) {
-  let prime = true;
-
-  for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
-      prime = false;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            prime = false;
+        }
     }
-  }
 
-  if (prime) {
-    console.log(num);
-  }
-}
-
-let a = 5;
-let b = 2;
-
-console.log(a % b);
-
-
-// print the multiple table of 9
-
-let num1 = 9;
-for (let i = 1; i <= 10; i++) {
-  console.log(`${num1} * ${i} = `, num1 * i);
-}
-
-const promise = async()  => {
-  try {
-const user = await user.create();
-console.log("okay")
-  } catch (error) {
-console.log("not okay")
-  }
-}
-
-
-// addition
-let a = 5;
-let b = 4;
-function add() {
-  return a + b;
-}
-
+    if (prime) {
+        console.log(num);
+    };
+};
